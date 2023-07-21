6050e0e4887911c7383cefa153a9bad8082b3941
Art Karasev <as.karasyov@gmail.com>
2023-07-21 12:36:46 +1200

    tweaks

--- client/src/extra/js/admin-ctrl.js
+++ client/src/extra/js/admin-ctrl.js
@@ -17,27 +17,27 @@ class AdminGear {
 
       let logoutLink = document.head.querySelector('meta[name="x-cms-logout-link"]');
 
-      let html = '\
-        <div class="adminctrl">\
+      let body = document.getElementsByTagName('body')[0];
+
+      let adminctrl = document.createElement('div');
+      adminctrl.classList.add('adminctrl');
+      adminctrl.innerHTML = '\
           <ul>\
             <li data-i="exit"><a href="' + logoutLink.content + '" title="Log out"></a></li>\
             <li data-i="planet"><a href="/admin" title="Admin panel"></a></li>\
             <li data-i="build"><a href="' + editLink.content + '" title="Edit this page"></a></li>\
           </ul>\
-        </div>\
       ';
 
-      let body = document.getElementsByTagName('body')[0];
-
-      body.innerHTML += html;
-
-      let adminctrl = document.getElementsByClassName('adminctrl')[0];
-
       adminctrl.addEventListener('mouseover', (e) => {
-        adminctrl.classList.add('adminctrl--active')
+
+        let element = document.getElementsByClassName('adminctrl')[0];
+        element.classList.add('adminctrl--active')
       });
       adminctrl.addEventListener('mouseout', (e) => {
-        adminctrl.classList.remove('adminctrl--active')
+
+        let element = document.getElementsByClassName('adminctrl')[0];
+        element.classList.remove('adminctrl--active')
       });
 
       setTimeout(() => adminctrl.classList.add('adminctrl--visible'), 100);
