36991d6e1f33769876fbf2bc93e9739444d51a7e
Art Karasev <as.karasyov@gmail.com>
2023-07-27 09:21:51 +1200

    restructure

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -3,8 +3,10 @@ const dev = import.meta.env && import.meta.env.DEV;
 const host = 'silverstripe-starter.lh';
 
 const bootstrap_icon_path = dev ? '/_resources/vendor/goldfinch/extra-assets/client/dist/bootstrap-icons/icons/' : '../node_modules/bootstrap-icons/font/fonts';
-const silverstripe_admin = dev ? 'silverstripe-admin/client/src/' : '../'
-const enchantment = dev ? 'enchantment/' : ''
+const silverstripe_admin = dev ? 'silverstripe-admin/client/src/' : '/_resources/vendor/goldfinch/enchantment/client/dist/enchantment/assets/'
+const enchantment = dev ? '/_resources/vendor/goldfinch/enchantment/client/dist/enchantment/assets/enchantment/' : ''
+
+const silverstripe_admin_font_path = dev ? (silverstripe_admin + 'font/fonts/') : '/_resources/vendor/silverstripe/admin/client/dist/fonts/';
 
 export default {
 
@@ -15,6 +17,7 @@ export default {
     $bootstrap-icons-font-dir: '${bootstrap_icon_path}';
     $silverstripe-admin: '${silverstripe_admin}';
     $enchantment: '${enchantment}';
+    $silverstripe-admin-font-path: '${silverstripe_admin_font_path}';
   `,
 
   public: {
