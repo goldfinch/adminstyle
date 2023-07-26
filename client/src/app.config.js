a798d574cfe8b535bb877ba0db8e753804367455
Art Karasev <as.karasyov@gmail.com>
2023-07-27 08:22:54 +1200

    tweaks

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -5,6 +5,7 @@ const host = 'silverstripe-starter.lh';
 const bootstrap_icon_path = dev ? '/node_modules/bootstrap-icons/font/fonts/' : '/_resources/vendor/goldfinch/extra-assets/client/dist/bootstrap-icons/icons/';
 const silverstripe_admin = dev ? '' : 'silverstripe-admin/client/src/'
 const enchantment = dev ? '' : 'enchantment/'
+
 export default {
 
   host: host,
